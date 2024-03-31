f07942e9810ff233988306a346937bcac8acf4e2
Vinicius Oliveira <abreu-vinicius15@hotmail.com>
2024-03-31 16:40:13 -0300

    Geração de JWT

--- BackEnd/src/models/cadUsersModels.js
+++ BackEnd/src/models/cadUsersModels.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const getAll = async () => {
   const [users] = await conn.execute('Select * from cadUsers');
   
-  const tokenGerado = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5vbWVAYWx1cmEuY29tLmJyIiwicGFzc3dvcmQiOiJIdUVLVzQ4OSFqNDQ1KiIsImlhdCI6MTY1MTY4MzUxNywiZXhwIjoxNjgzMjQxMTE3LCJzdWIiOiIxIn0.t0UuIAxJ1NPXANtlBOKfHfLsePF4LRPu4RA2WMkJl6A';
+  const tokenGerado = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5vbWVAYWx1cmEuY29tLmJyIiwicGFzc3dvcmQiOiJIdUVLVzQ4OSFqNDQ1KiIsInBhcHBlciI6IjEiLCJpYXQiOjE3MTE5MTIzNzcsImV4cCI6MTc0MzQ2OTk3Nywic3ViIjoiMSJ9.FaL51bRyj5Pl1NaJ8g5fQPqu_QKt_hC5ioOCzjNUisg';
   console.log(jwt.decode(tokenGerado));
   return users;
 };
@@ -14,25 +14,24 @@ const cadUsers = async (user) => {
   const query = 'INSERT INTO cadUsers(Uname,Ucpf,Upaper,Uuf,UconsPro,Uemail,Upass) VALUES (?,?,?,?,?,?,?)';
   const [createdUser] = await conn.execute(query,[Uname,Ucpf,Upaper,Uuf,UconsPro,Uemail,Upass]);
 
-  const secretKey = 'skljaksdj9983498327453lsldkjf';
+                     //clicnpj_soroteca_2024** 
+  const secretKey = '44d939129da1372c74eb4798ffd930cc';
+                     
 
-  const nossoToken = jwt.sign(
+  const makeTokem = jwt.sign(
     {
-      email: 'nome@alura.com.br',
-      password: 'HuEKW489!j445*',
+      papper: Upaper
     },
     secretKey,
     {
       expiresIn: '1y',
-      subject: '1',
     }
   );
 
-  console.log(nossoToken);
+  console.log(makeTokem);
 
   return {insertedId: createdUser.insertId};
 }
-
 
 
 const deleteUser = async (id) =>{

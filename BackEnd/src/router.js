e4f435c8ace142bac8613e234b8fbf85aec6dc14
Vinicius Oliveira <abreu-vinicius15@hotmail.com>
2024-04-01 21:04:30 -0300

    Criando documentação do projeto.

--- BackEnd/src/router.js
+++ BackEnd/src/router.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cadUser = require('./controllers/cadController');
 const getRules = require('./controllers/rulesController.js');
 
+const swaggerUi = require('swagger-ui-express');
+const  swaggerDocument  =  require ('../swagger.json');
+
+
 const cadUsersMiddleware = require('./middleWares/cadUsesrMiddleware.js');
 const router = express.Router();
+router.use('/soroteca-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument)) ;
 
 router.get('/rules', getRules.getAllRules);
 router.get('/cadUsers', cadUser.getAll);

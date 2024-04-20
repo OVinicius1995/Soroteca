acf4e15ed39fee114d3704a452193bc850f0eacb
Vinicius Oliveira <abreu-vinicius15@hotmail.com>
2024-04-20 11:33:27 -0300

    Grid do cadastro de amostras

--- FrontEnd/JavaScript/soroteca.js
+++ FrontEnd/JavaScript/soroteca.js
@@ -63,120 +63,218 @@ function showCadAmostras(){
   wichRack.appendChild(makeFirstOption)
   makeFirstOption.value = "Escolha";
   makeFirstOption.text = "Irá para qual rack:";
+
+  cadGal.classList.remove('active');
+  cadRac.classList.remove('active');
+  cadAmo.classList.toggle('activeAmostras');
   
-  if(datasRacks.length === 0){
+  // if(datasRacks.length === 0){
     
-    Swal.fire({
-      icon: "error",
-      title: "Nenhum rack encontrado.",
-      text: "Não há racks cadastrados, antes de cadastrar as amostras é preciso que ajam racks, verifique!",
-  });
+  //   Swal.fire({
+  //     icon: "error",
+  //     title: "Nenhum rack encontrado.",
+  //     text: "Não há racks cadastrados, antes de cadastrar as amostras é preciso que ajam racks, verifique!",
+  // });
 
-  } else{
+  // } else{
     
-    cadGal.classList.remove('active');
-    cadRac.classList.remove('active');
-    cadAmo.classList.toggle('activeAmostras');
+  //   cadGal.classList.remove('active');
+  //   cadRac.classList.remove('active');
+  //   cadAmo.classList.toggle('activeAmostras');
 
-    datasRacks.forEach((dadosRack,index) => {
-      let   makeOption = document.createElement('option');
-      let rackColuns = dadosRack.rackNumberColuns;
-      let rackLines  = dadosRack.rackNumberLines;
-      let rackName =  dadosRack.rackName;
+  //   datasRacks.forEach((dadosRack,index) => {
+  //     let   makeOption = document.createElement('option');
+  //     let rackColuns = dadosRack.rackNumberColuns;
+  //     let rackLines  = dadosRack.rackNumberLines;
+  //     let rackName =  dadosRack.rackName;
 
-      makeOption.value = rackName;
-      makeOption.text = rackName;
+  //     makeOption.value = rackName;
+  //     makeOption.text = rackName;
       
-      wichRack.appendChild(makeOption);
+  //     wichRack.appendChild(makeOption);
 
-      console.log({
-        rackColuns,
-        rackLines,
-        rackName
-      });   
-  });
+  //     console.log({
+  //       rackColuns,
+  //       rackLines,
+  //       rackName
+  //     });   
+  // });
+  // }
+}
+
+// function getDadosRackOnchange(){
+
+//   let table      = document.getElementById("myTable");
+
+//   datasRacks.filter((dadosRack,index) => {
+
+//     if(wichRack.value === dadosRack.rackName){
+      
+//       let rackColuns = dadosRack.rackNumberColuns;
+//       let rackLines  = dadosRack.rackNumberLines;
+//       let rackName =  dadosRack.rackName;
+      
+//       console.log({
+//         rackColuns,
+//         rackLines,
+//         rackName
+//       }); 
+//       stop;
+//       let testePreenche = document.getElementById('teste');
+//       let testePreenche1 = document.getElementById('teste1');
+//       let testePreenche2 = document.getElementById('teste2');
+//       let getGrid    = document.getElementById('gridAmostras');
+//       let getBody    = document.querySelector('tbody');
+
+      
+
+//       for (let i = 0; i < rackColuns; i++) {
+//         let makeColumn = document.createElement('th');
+//         getGrid.appendChild(makeColumn);
+//         makeColumn.setAttribute("id",i);
+//         makeColumn.innerText="Column " + parseFloat(i) * 1;
+        
+//       }
+
+//     //   <tr>
+//     //   <td>Linha 1</td> 
+//     //   <td>Alfreds Futterkiste</td>
+//     //   <td>Maria Anders</td>
+//     //   <td>Germany</td>
+//     // </tr>
+
+//       for (let i = 0; i < rackLines; i++) {
+//         let makeLines = document.createElement('tr');
+//         let makeTd    = document.createElement('td');
+//         let getLinha    = document.querySelector('tr');
+//         // 
+
+//         // getGrid.appendChild(makeLines);
+//         getBody.appendChild(makeLines); 
+//         makeLines.appendChild(makeTd);
+//         makeLines.setAttribute("id","tr"+i);
+//         makeTd.innerText="Linha " + i;
+//         // makeLines.appendChild(makeTd);       
+//         // makeTd.setAttribute("id","linha"+i);
+//         // makeTd.innerText="Teste" + i;        
+//       }
+
+      
+
+//       for (let i = 0; i < rackLines; i++) {
+
+//         let getTr      = document.getElementById("tr"+i);
+        
+//         // let makeCels   = document.createElement('td');
+//         let makeCels2   = document.createElement('td');
+//         let makeCels3   = document.createElement('td');
+//         let makeCels4   = document.createElement('td');
+//         let a = 0;
+//         let b =rackLines * rackColuns;
+
+//         //getTr.appendChild(makeCels); 
+        
+//         // var row = table.insertRow(proximaLinha);
+        
+
+//         do {
+          
+//           let makeCels   = document.createElement('td');
+//           getTr.appendChild(makeCels);
+//          // makeCels.setAttribute("id","td"+a,onclick="myFunction(this)");
+//           makeCels.setAttribute("onclick",`myFunction(${a})`);
+//           makeCels.textContent="idLinha","td"+a,"idColumn","cl" + b--;
+//           makeCels.innerText="Conteudo linha" + i + "col" + i;       
+          
+//           a++ 
+
+//         } while (a <4);
+//       }
+
+//       var proximaLinha = table.rows.length
+//       alert(proximaLinha)
+
+//       // testePreenche.innerText  = rackName;
+//       // testePreenche1.innerText = rackColuns;
+//       // testePreenche2.innerText = rackLines;
+//     }
+// });
+// }
+
+// function myFunction(x,y) {
+  
+//   alert("Cell index is: " + x.cellIndex);
+// }
+
+function addLinha() {
+  //Recupera a tabela onde serão adicionado as linhas
+  // var table = document.getElementById("myTable");
+  var tbody = document.querySelector("tbody");
+  var thInic = document.createElement("th");
+  tbody.appendChild(thInic);
+  for(var c = 0; c < 5; c++) {
+    var th     = document.createElement("th");
+    tbody.appendChild(th);
+    th.innerText="Coluna";
+  }
+  for(var c = 0; c < 5; c++) {
+    var tdLines = document.createElement("td");
+    var trLinas = document.createElement("tr");
+    tbody.appendChild(trLinas);
+    trLinas.appendChild(tdLines)
+    tdLines.innerText="Linhas";
+    trLinas.setAttribute("id","l"+c);
+  }
+
+  for(var c = 0; c < 5; c++) {
+    // var trLinhas = document.createElement("tr");
+    // var tdLinhas = document.createElement("td");
+    // var td = document.createElement("td");
+    // var rowLinhas  = table.insertRow(0);
+    // // var cell0  = row.insertCell(0);
+    
+    // // tbody.appendChild(trLinhas);
+    // // trLinhas.appendChild(tdLinhas);
+    // // cell0.appendChild(td);
+    // rowLinhas.innerText="Linhas";
+    
+    var table = document.getElementById('l'+c);
+
+        //recupera a quantidade atual de linhas da tabela, para inserir a nova linha na proxima posição disponivel
+        var proximaLinha = 0
+        // var row     = table.insertRow(proximaLinha);
+        // var rowInic = table.insertRow(proximaLinha);
+        
+        //Como serão 5 células, um for para criar cada uma das células e os textareas
+        for(var d = 1; d < 6; d++) {
+          //cria uma nova célula para a linha
+          
+          let cels = table.insertCell(d);
+          
+          //Cria um novo elemento textarea
+          // var cels = document.createElement("td");
+          
+          //Define a propriedade name
+          //o nome será unico para cada textarea no formato "campo_NUMEROLINHA_NUMEROCOLUNA"
+          cels.className  = "linha_" + table.rowIndex + "coluna_" + cels.cellIndex;
+                              
+          //Altera o style do textarea definindo a propriedade overflow
+          cels.setAttribute("onclick","myFunction(this.className)")
+          cels.innerText="TESTESSS" + c;
+          //Insere o textarea criado dentro da celula
+          // cell0.appendChild(cels);
+        }     
   }
 }
 
-function getDadosRackOnchange(){
+function myFunction(x,y) {
+  alert(x)    
+  let valor = x;
+  let teste = document.querySelector('.'+valor);
+  console.log(teste.innerText)
 
-  alert(wichRack.value)
-
-  datasRacks.filter((dadosRack,index) => {
-
-    if(wichRack.value === dadosRack.rackName){
-      alert("Encontrou");
-      let rackColuns = dadosRack.rackNumberColuns;
-      let rackLines  = dadosRack.rackNumberLines;
-      let rackName =  dadosRack.rackName;
-      
-      console.log({
-        rackColuns,
-        rackLines,
-        rackName
-      }); 
-      stop;
-      let testePreenche = document.getElementById('teste');
-      let testePreenche1 = document.getElementById('teste1');
-      let testePreenche2 = document.getElementById('teste2');
-      let getGrid    = document.getElementById('gridAmostras');
-      let getBody    = document.querySelector('tbody');
-
-      
-
-      for (let i = 0; i < rackColuns; i++) {
-        let makeColumn = document.createElement('th');
-        getGrid.appendChild(makeColumn);
-        makeColumn.setAttribute("id",i);
-        makeColumn.innerText="Column " + parseFloat(i) * 1;
-        
-      }
-
-    //   <tr>
-    //   <td>Linha 1</td> 
-    //   <td>Alfreds Futterkiste</td>
-    //   <td>Maria Anders</td>
-    //   <td>Germany</td>
-    // </tr>
-
-      for (let i = 0; i < rackLines; i++) {
-        let makeLines = document.createElement('tr');
-        let makeTd    = document.createElement('td');
-        let getLinha    = document.querySelector('tr');
-        // 
-
-        // getGrid.appendChild(makeLines);
-        getBody.appendChild(makeLines); 
-        makeLines.appendChild(makeTd);
-        makeLines.setAttribute("id","tr"+i);
-        makeTd.innerText="Linha " + i;
-        // makeLines.appendChild(makeTd);       
-        // makeTd.setAttribute("id","linha"+i);
-        // makeTd.innerText="Teste" + i;        
-      }
-
-      alert(rackLines * rackColuns)
-
-      for (let i = 0; i < rackLines; i++) {
-
-        let getTr      = document.getElementById("tr"+i);
-        let makeCels   = document.createElement('td');
-        let makeCels2   = document.createElement('td');
-        let makeCels3   = document.createElement('td');
-        let makeCels4   = document.createElement('td');
-
-        getTr.appendChild(makeCels); 
-        getTr.appendChild(makeCels2); 
-        getTr.appendChild(makeCels3); 
-        getTr.appendChild(makeCels4); 
-        makeCels.innerText="Conteudo linha" + 1 + "col" + i;       
-      }
-
-      // testePreenche.innerText  = rackName;
-      // testePreenche1.innerText = rackColuns;
-      // testePreenche2.innerText = rackLines;
-    }
-});
+  document.querySelector('.linha_3coluna_1').innerText = "Não foi automático, foi na mão."
+  // alert("Cell index is: " + x.cellIndex + " " + row.rowIndex + "_" + cell0.cellIndex);
 }
 
 function cadGalery(event){
